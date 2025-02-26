ebb94b03d3fb3f6244a2340a1f5a8d4571f7fa44
Aneesh Anandanatarajan <156273718+AneeshAnandanatarajan@users.noreply.github.com>
2025-02-26 19:29:17 +0000

    Ex5 Updated

--- UserInput-ex5.cpp
+++ UserInput-ex5.cpp
@@ -1,1 +1,25 @@
 // Add your code here
+#include <iostream>
+using namespace std;
+
+int factorial (int n)
+{
+    int i = 1;
+    int fact = n;
+    while ((n - i) > 0)
+    {
+        fact = fact * (n-i);
+        ++i;
+    }
+    return fact;
+}
+
+
+int main()
+{
+    int i;
+    cout << "Please enter a number:\n";
+    cin >> i;
+    cout << "The factorial of your number is " << factorial(i) << "\n";
+    return 0;
+}
