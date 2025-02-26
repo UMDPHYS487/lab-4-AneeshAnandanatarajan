8162b7a3b92d5ff6024a09715f5735fee6a1ebf8
Aneesh Anandanatarajan <156273718+AneeshAnandanatarajan@users.noreply.github.com>
2025-02-26 19:21:29 +0000

    Ex5 Update

--- FactorialLoop-ex4.cpp
+++ FactorialLoop-ex4.cpp
@@ -1,1 +1,26 @@
 // Add your code here
+#include <iostream>
+using namespace std;
+
+int factorial (int n)
+{
+    int i = 1;
+    int fact = n;
+    while ((n - i) > 0)
+    {
+        fact = fact * (n-i);
+        ++i;
+    }
+    return fact;
+}
+
+
+int main()
+{
+    int n = 1;
+    while (n < 10)
+    {
+        cout << factorial(n) << "\n";
+        ++n;
+    }
+}
