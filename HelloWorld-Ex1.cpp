b536fefe930763316239b90f3fe0cc5c900f25d5
Aneesh Anandanatarajan <156273718+AneeshAnandanatarajan@users.noreply.github.com>
2025-02-26 18:27:49 +0000

    EX1 Update

--- HelloWorld-Ex1.cpp
+++ HelloWorld-Ex1.cpp
@@ -1,1 +1,7 @@
 // insert your code here
+#include <iostream>
+
+int main()
+{
+    std::cout << "Boing\n";
+}
--- Loop-ex2.cpp
+++ Loop-ex2.cpp
@@ -1,1 +1,14 @@
 // add your code here
+#include <iostream>
+
+int main()
+
+{
+    int n = 0;
+    while (n < 10) 
+    {
+        cout << n << ", ";
+        ++n;
+    }
+    cout << "Done";
+}
